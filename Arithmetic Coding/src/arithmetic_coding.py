41029713f15f47a4201900a2f9d3a31063bc32a6
Caroline Alves <sma.caroline@ymail.com>
2025-07-03 10:54:02 -0300

    File reorganization

--- Arithmetic Coding/src/arithmetic_coding.py
+++ Arithmetic Coding/src/arithmetic_coding.py
@@ -79,3 +79,122 @@ class CodeCandidateList:
         for i, candidate in enumerate(self.list):
             print(f"Candidate {i + 1}:")
             candidate.print(candidate)
+
+def cumulative_prob(P):
+    """Cria os limites cumulativos para cada símbolo"""
+    cum = {}
+    total = 0.0
+    for symbol, prob in sorted(P.items()):
+        cum[symbol] = (total, total + prob)
+        total += prob
+    return cum
+
+def update_code_interval(code_intervals, P):
+    code_interval_list = CodeCandidateList(2 * code_intervals.k)
+    probability = P[0]
+    for cc in code_intervals.list:
+        range_ = cc.upperBound - cc.lowerBound
+
+        if cc.symbols[0] == 0:
+            l2 = cc.upperBound - (1 - probability) * range_
+            
+            # Novo objeto para símbolo 0
+            c0 = CodeCandidate()
+            c0.symbols = cc.symbols + [0]
+            c0.upperBound = l2
+            c0.lowerBound = cc.lowerBound
+            c0.probability = c0.upperBound - c0.lowerBound
+            code_interval_list.list.append(c0)
+
+            # Novo objeto para símbolo 1
+            c1 = CodeCandidate()
+            c1.symbols = cc.symbols + [1]
+            c1.upperBound = cc.upperBound
+            c1.lowerBound = l2
+            c1.probability = c1.upperBound - c1.lowerBound
+            code_interval_list.list.append(c1)
+        
+        elif cc.symbols[0] == 1:
+            u2 = cc.lowerBound + (probability) * range_
+            
+            # Novo objeto para símbolo 0
+            c0 = CodeCandidate()
+            c0.symbols = cc.symbols + [0]
+            c0.upperBound = u2
+            c0.lowerBound = cc.lowerBound
+            c0.probability = c0.upperBound - c0.lowerBound
+            code_interval_list.list.append(c0)
+
+            # Novo objeto para símbolo 1
+            c1 = CodeCandidate()
+            c1.symbols = cc.symbols + [1]
+            c1.upperBound = cc.upperBound
+            c1.lowerBound = u2
+            c1.probability = c1.upperBound - c1.lowerBound
+            code_interval_list.list.append(c1)
+
+    return code_interval_list
+
+def matching_canditade_intervals(source_interval, code_interval_list):
+    """Encontra os intervalos de símbolos que coincidem com os da fonte"""
+    
+    for cc in code_interval_list.list:
+        if source_interval.lowerBound >= cc.lowerBound and source_interval.upperBound <= cc.upperBound:
+            return True, cc
+    return False, None
+
+def find_code_interval_from_candidates(cc_list, searchList):
+    """
+    Finds the interval corresponding to a given sequence of symbols.
+    """
+    code_interval = Interval()
+    for cc in cc_list.list:
+        if cc.symbols == searchList:
+            code_interval.lowerBound = cc.lowerBound
+            code_interval.upperBound = cc.upperBound
+            searchList.clear()     
+    return code_interval
+
+def output_and_rescale(source_interval, code_intervals, P_code):
+
+    code_symbols = []
+    cc = CodeCandidate()
+    
+    success, cc = matching_canditade_intervals(source_interval, code_intervals)
+    
+    while success:
+        range_ = cc.upperBound - cc.lowerBound
+        source_interval.lowerBound = (source_interval.lowerBound  - cc.lowerBound)/ range_
+        source_interval.upperBound = (source_interval.upperBound- cc.lowerBound)/ range_
+        #source_interval = rescale_source_interval(source_interval, new_low, new_high)
+        print("\nIntervalo encontrado")
+        print("Intervalo Fonte:")
+        source_interval.print()
+        print("Intervalo Candidato:") 
+        cc.print_interval()
+        code_symbols.extend(cc.symbols)
+        
+    
+        code_intervals = update_code_interval(code_intervals, P_code)
+        
+        success, cc = matching_canditade_intervals(source_interval, code_intervals)
+
+    return code_symbols, source_interval, code_intervals
+
+def matching_canditade_intervals_finalize(source_interval, code_interval_list, P_code):
+    """Encontra os intervalos de símbolos que coincidem com os da fonte"""
+    new_code_interval_list = CodeCandidateList(code_interval_list.k)
+    fin_intervals = {}
+    fin_symbols = []
+ 
+    for cc in code_interval_list.list:
+        if (source_interval.lowerBound < cc.upperBound) and (source_interval.upperBound > cc.lowerBound):
+            new_code_interval_list.list.append(cc)
+    new_code_interval_list.k = len(new_code_interval_list.list)
+    new_code_interval_list = update_code_interval(new_code_interval_list, P_code)
+
+    for cc in new_code_interval_list.list:
+        if (source_interval.lowerBound < cc.lowerBound) and (source_interval.upperBound > cc.upperBound):
+            fin_intervals[cc.probability] = cc.symbols
+    fin_symbols = fin_intervals[max(fin_intervals)]
+    return fin_symbols
