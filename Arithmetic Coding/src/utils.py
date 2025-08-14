b88ec58586fdf35b50b16bd7b1397a134f7c0ff2
Caroline Alves <sma.caroline@ymail.com>
2025-08-14 11:42:42 -0300

    remove files

--- Arithmetic Coding/src/utils.py
+++
@@ -1,9 +0,0 @@
-import arithmetic_coding as ac
-
-def show_results(symbol, code_interval, source_interval): 
-    
-    print(f"\nSímbolo Atual {[symbol]}")  
-    print("Intervalo Candidato:")
-    code_interval.print()
-    print("Intervalo Fonte:")
-    source_interval.print()
